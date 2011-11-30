e10788fa15b4ef670bdc73d62b2335aa192ff14d
Randar Labs <randar.labs@gmail.com>
2011-11-30 17:13:53 -0600

    Added an Android Splash Screen demo
    Can be used while your app loads. Will only keep the splash screen up
    while the app loads and won't be shown again if the back button is used.

---
+++ src/com/randarlabs/android/MyApp.java
@@ -0,0 +1,13 @@
+package com.randarlabs.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+ 
+public class MyApp extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
